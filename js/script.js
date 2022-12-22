e63b9723b5ddb80c9b54365c6cd1fa9684974cf8
omen <kybar089@gmail.com>
2022-12-22 00:18:17 -0600

    created checkAnswer function and click listeners for answer options

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ var opt3 = document.getElementById("option-3")
 var opt4 = document.getElementById("option-4")
 var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("main");
+var answerCheck = document.getElementById("checkAnswer");
 var questionIndex = 0
-
 var secondsLeft = 75;
 
 function setTime() {
@@ -22,45 +22,28 @@ function setTime() {
     }, 7500);
 }
 
-
-
-
-
-
-
-
-
-
-
-
 var questions = [
         {
             question: "Commonly used data types DO NOT include:",
-            options: ["1.strings", "2.booleans", "3. alerts", "4. numbers"],
-            correctAnswer: "3.alerts"
+            options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+            correctAnswer: "3. alerts"
         },{
-            question : "The condition in an if/else statement is enclosed within _____.",
-            options: ["quotes", "2.curly brackets", "3. parenthesis", "4.square brackets"],
-            correctAnswer: "3.parenthesis"
+            question: "The condition in an if/else statement is enclosed within _____.",
+            options: ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+            correctAnswer: "3. parenthesis"
         },{
-            question : "Arrays in JavaScript can be used to store _____.",
-            option1 : "numbers and strings",
-            option2 : "other arrays",
-            option3 : "booleans",
-            option4 : "all of the above"
+            question: "Arrays in JavaScript can be used to store _____.",
+            options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+            correctAnswer: "4. all of the above"
         },{
-            question : "String values must be enclosed within ___ when being assigned to variables.",
-            option1 : "commas",
-            option2 : "curly brackets",
-            option3 : "quotes",
-            option4 : "parenthesis"
+            question: "String values must be enclosed within ___ when being assigned to variables.",
+            options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+            correctAnswer: "3. quotes"
         },{
-            question : "A very useful tool used during development and debugging for printing content to the debugger is:",
-            option1 : "JavaScript",
-            option2 : "terminal/bash",
-            option3 : "for loops",
-            option4 : "console log"
-        },
+            question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+            options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"],
+            correctAnswer: "4. console log"
+        }
     ];
     
 function renderQuestion() {
@@ -70,28 +53,44 @@ function renderQuestion() {
     opt2.textContent = questions[questionIndex].options[1];
     opt3.textContent = questions[questionIndex].options[2];
     opt4.textContent = questions[questionIndex].options[3];
-
-    // var optionsArray = questions[0].options
-    // console.log(optionsArray)
-    // var q = questions[0];
-    // var o = questions[0].options
-  
-
-    // question.innerHTML = "<p>" + q.question + "</p>";
-    // opt1.innerHTML = "<p>" + optionsArray[0] + "</p>"
-    // opt2.innerHTML = "<p>" + optionsArray[1] + "</p>"
-    // opt3.innerHTML = "<p>" + optionsArray[2] + "</p>"
-    // opt4.innerHTML = "<p>" + optionsArray[3] + "</p>"
 }
 
 function checkAnswer(answer) {
-    
+    var lineBreak = document.getElementById("lineBreak");
+    lineBreak.style = "block";
+    checkAnswer.style = "block";
+
+    if (questions[questionIndex].answer ===
+        questions[questionIndex].options[answer]) {
+            correctAns++;
+            checkAnswer.textContent = "Correct!!!";
+        } else {
+            checkAnswer.textContent = "Incorrect! The answer is: " + questions[questionIndex].answer;
+        }
+
+        questionIndex++;
+        if (questionIndex < questions.length) {
+            renderQuestion();
+        } else {
+            console.log("GameOver")
+        }
+        
+
 }
 
+function chose1() { checkAnswer(0) }
+function chose2() { checkAnswer(1) }
+function chose3() { checkAnswer(2) }
+function chose4() { checkAnswer(3) }
 
 function startQuiz() {
     start.style.display = "none";
     renderQuestion();
 }
+
 startBtn.addEventListener("click", startQuiz)
+opt1.addEventListener("click", chose1);
+opt2.addEventListener("click", chose2);
+opt3.addEventListener("click", chose3);
+opt4.addEventListener("click", chose4);
 
